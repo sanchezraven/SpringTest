1a750345ed859f91db915c8d5a5ef34bb2dd3940
alex_voronin <sanchez_raven@mail.ru>
2024-12-22 17:56:39 +0500

    added injections String from property file

--- src/main/java/com/udemu/demo/Person.java
+++ src/main/java/com/udemu/demo/Person.java
@@ -2,9 +2,27 @@ package com.udemu.demo;
 
 public class Person {
     private Pet pet;
+    private String surname;
+    private int age;
 
     public Person() {
         System.out.println("Person is created");
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 
     public void callYourPet() {
--- src/main/java/com/udemu/demo/Test3.java
+++ src/main/java/com/udemu/demo/Test3.java
@@ -7,6 +7,9 @@ public class Test3 {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         Person person = context.getBean("myPerson", Person.class);
         person.callYourPet();
+
+        System.out.println(person.getSurname());
+        System.out.println(person.getAge());
         context.close();
     }
 }
